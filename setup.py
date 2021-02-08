a7553e5b51fbecaafa65f773a9439d5c10c9774c
bodhi <jyxz5@qq.com>
2021-02-08 12:46:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 # coding: utf-8
 import setuptools, os
+import sys
 
 packagename= "pyner"
-version = "0.0.1"
+version ="0.0.8"
 path = os.path.join(os.getcwd(),packagename, "README.md")
+requiredments = os.path.join(os.getcwd(),packagename, "requirements.txt")
 
 with open(path, "r", encoding='utf-8') as fh:
     long_description = ''.join(fh.readlines()[:-1])
+
+with open(requiredments, "r", encoding='utf-8') as fh:
+    requiredpackages = [ line.strip() for line in fh.readlines() ]
+
+#print(requiredpackages)
 
 setuptools.setup(
     name=packagename,
     version=version,
     author="Wang Bodhi Faqun",
     author_email="jyxz5@hotmail.com",
-    description="This program is SDK of Ner service API to handle one sentence or simple text file in chinese.",
+    description="This program is SDK of NER service API to handle one sentence or simple text file.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tjnh05/ner",
@@ -23,12 +30,13 @@ setuptools.setup(
 
     packages=setuptools.find_packages(),
     package_data={
-        packagename: ['test.txt','README.md'],
+        'pyner': ['test.txt','requirements.txt']
     },
+    install_requires=requiredpackages,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.5.2',
 )
